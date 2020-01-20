3f894c687c4dcf152ff5d9cb77dcda2d723ca8f6
wzwmzm <wzw_mzm@aliyun.com>
2020-01-20 20:07:19 +0800

    a

--- misc/rc.local.g808.sh
+++ misc/rc.local.g808.sh
@@ -2,7 +2,10 @@ sudo /bin/hostname -b g808;
 
 
 #sudo setcap CAP_NET_BIND_SERVICE=+eip "/home/wzw/project/go/src/html/9fileserver(net.http)/fileserver.arm"
-"/home/wzw/project/go/src/html/9fileserver(net.http)/"fileserver.arm &
+"/home/wzw/project/go/src/2html/9fileserver(net.http)/"fileserver.arm &
+
+#PWA
+/home/wzw/project/go/src/2html/15pwa/2workbox/laboratory.arm &
 #<----------------注意: 选用上面, 或是下面的!!!
 #选用下面的须建立目录 mkdir ~/p ;并拷贝 fileserver-arm 文件
 #sudo setcap CAP_NET_BIND_SERVICE=+eip "/home/wzw/p/fileserver.arm"
@@ -22,7 +25,7 @@ sudo /bin/hostname -b g808;
 sudo setcap CAP_NET_BIND_SERVICE=+eip /usr/local/bin/caddy
 #5, 运行 : caddy -port 44444 -conf  /home/wzw/project/go/src/html/8iris/7proxy-caddy/Caddyfile
 #   -port 44444  是指caddy作为web服务器运行在44444端口
-caddy -port 44444 -conf  /home/wzw/project/go/src/html/8iris/7proxy-caddy/Caddyfile &
+caddy -port 44444 -conf  /home/wzw/project/go/src/2html/8iris/7proxy-caddy/Caddyfile &
 
 #这样改没有用
 ##删除一个可疑进程，并创建一个占位符
