b93ef6477ae35a7d3ada60457546957e38becb8b
wzwmzm <wzw_mzm@aliyun.com>
2020-03-02 14:27:28 +0800

    a

--- misc/rc.local.g808.sh
+++ misc/rc.local.g808.sh
@@ -2,8 +2,16 @@ sudo /bin/hostname -b g808;
 
 
 #PWA
+#port:8100
 cd /home/wzw/project/go/src/2html/15pwa/2workbox/
 "/home/wzw/project/go/src/2html/15pwa/2workbox/laboratory.arm" &
+
+
+#移动资管
+#prot:8101
+cd  "/home/wzw/project/go/src/2html/20设备管理"
+go run example.go &
+
 
 #<----------------注意: 选用上面, 或是下面的!!!
 #选用下面的须建立目录 mkdir ~/p ;并拷贝 fileserver-arm 文件
@@ -12,6 +20,7 @@ cd /home/wzw/project/go/src/2html/15pwa/2workbox/
 
 
 #sudo setcap CAP_NET_BIND_SERVICE=+eip "/home/wzw/project/go/src/html/9fileserver(net.http)/fileserver.arm"
+#port:8888
 "/home/wzw/project/go/src/2html/9fileserver(net.http)/"fileserver.arm &
 
 
@@ -27,7 +36,7 @@ cd /home/wzw/project/go/src/2html/15pwa/2workbox/
 sudo setcap CAP_NET_BIND_SERVICE=+eip /usr/local/bin/caddy
 #5, 运行 : caddy -port 44444 -conf  /home/wzw/project/go/src/html/8iris/7proxy-caddy/Caddyfile
 #   -port 44444  是指caddy作为web服务器运行在44444端口
-caddy -port 44444 -conf  /home/wzw/project/go/src/2html/8iris/7proxy-caddy/Caddyfile &
+caddy -port 44444 -conf  /home/wzw/project/go/src/misc/7proxy-caddy/Caddyfile &
 
 #这样改没有用
 ##删除一个可疑进程，并创建一个占位符
