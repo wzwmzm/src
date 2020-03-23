b4c232eec22c5ec704a799ba9a6bb12193a09714
wzwmzm <wzw_mzm@aliyun.com>
2020-03-24 01:20:12 +0800

    a

--- 2html/20设备管理/web/js/index.js
+++ 2html/20设备管理/web/js/index.js
@@ -232,9 +232,93 @@ let scanner2 = new Instascan.Scanner({
 	refractoryPeriod: 5000, //连续识别相同QR码之前的时间
 	scanPeriod: 1 //两次扫描之间的周期
 });
+
+function str2utf8(str)
+{
+	// UCS-2和UTF8都是unicode的一种编码方式
+	// js代码中使用的是UCS-2编码
+
+	var code;
+	var utf = "";
+
+	for (var i = 0; i < str.length; i++)
+	{
+		code = str.charCodeAt(i);//返回每个字符的Unicode 编码
+
+		if (code < 0x0080) {
+			utf += str.charAt(i);//返回指定位置的字符
+		}
+		else if (code < 0x0800) {
+			utf += String.fromCharCode(0xC0 | ((code >> 6) & 0x1F));
+			utf += String.fromCharCode(0x80 | ((code >> 0) & 0x3F));
+		}
+		else if (code < 0x10000) {
+			utf += String.fromCharCode(0xE0 | ((code >> 12) & 0x0F));
+			utf += String.fromCharCode(0x80 | ((code >>  6) & 0x3F));
+			utf += String.fromCharCode(0x80 | ((code >>  0) & 0x3F));
+		}
+		else
+		{
+			throw "不是UCS-2字符集"
+		}
+
+	}
+	return utf;
+}
+
+function utf82str(utf) {
+	var str = "";
+	var tmp;
+
+	for (var i = 0; i < utf.length; i++) {
+		// 英文字符集合
+		if (utf.charCodeAt(i) >> 7 == 0x00) {
+			str += utf.charAt(i);
+			continue;
+		}
+		// 其他字符集
+		else if (utf.charCodeAt(i) >> 5 == 0x06) {
+			tmp = ((utf.charCodeAt(i + 0) & 0x1f) << 6) |
+				((utf.charCodeAt(i + 1) & 0x3f) << 0);
+			str += String.fromCharCode(tmp);
+			i++;
+			continue;
+		}
+		// 中文字符集
+		else if (utf.charCodeAt(i) >> 4 == 0x0e) {
+			tmp = ((utf.charCodeAt(i + 0) & 0x0f) << 12) |
+				((utf.charCodeAt(i + 1) & 0x3f) << 6) |
+				((utf.charCodeAt(i + 2) & 0x3f) << 0);
+			str += String.fromCharCode(tmp);
+			i += 2;
+			continue;
+		}
+		// 其他字符集
+		else if (utf.charCodeAt(i) >> 3 == 0x1f) {
+			tmp = ((utf.charCodeAt(i + 0) & 0x07) << 18) |
+				((utf.charCodeAt(i + 1) & 0x3f) << 12) |
+				((utf.charCodeAt(i + 2) & 0x3f) << 6);
+			((utf.charCodeAt(i + 3) & 0x3f) << 0);
+			str += String.fromCharCode(tmp);
+			i += 3;
+			continue;
+		}
+		// 非法字符集
+		else {
+			throw "不是UTF-8字符集"
+		}
+	}
+
+	return str;
+}
+
+
+
 scanner2.addListener('scan', function (data) {
-	let content = String.fromCharCode(data);
-	alert(content); //<----扫描结果
+	let content = str2utf8(data);
+	alert("str2utf8(str)",content); //<----扫描结果
+	content = utf82str(data);
+	alert("utf82str(data)",content);
 	
 	//<----可以调用scanner.stop()结束扫描
 
