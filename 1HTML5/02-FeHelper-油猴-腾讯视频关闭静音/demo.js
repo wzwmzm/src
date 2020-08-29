3e04a0fbd1bd19c14ef64b9c5057733976f2b7a8
wzwmzm <wzwmzm41@gmail.com>
2020-08-29 18:31:42 +0800

    a

---
+++ 1HTML5/02-FeHelper-油猴-腾讯视频关闭静音/demo.js
@@ -0,0 +1,38 @@
+// ==FeHelperMonkey==
+// @reminder        请不要删除这部分代码注释，这是FeHelper油猴脚本能正常工作的基本条件！当然，你可以按需修改这里的内容！
+// @id              mf_1598692144021
+// @name            专为腾讯视频播放时关闭静音
+// @url-pattern     https://v.qq.com/*
+// @enable          true
+// @require-js      
+// @auto-refresh    0
+// @updated         2020-08-29 18:00:33
+// ==/FeHelperMonkey==
+
+
+(function(){
+  
+  $(document).ready(function(){
+    
+    console.log("油猴开始了1");
+    console.log("油猴开始了1");
+    console.log("油猴开始了1");
+    console.log("油猴开始了1");
+    console.log("油猴开始了1");
+    console.log("油猴开始了1");
+    
+      $('txpdiv.txp_btn.txp_btn_volume').click();	//在页面载入完成后关闭静音
+    
+
+});
+  
+  $(()=>{
+	console.log("油猴开始了");
+    console.log("油猴开始了");
+    console.log("油猴开始了");
+    console.log("油猴开始了");
+    console.log("油猴开始了");
+    console.log("油猴开始了");
+  })
+
+})();
