c92c7fc602640910d96c1e917be394b6a108b7b3
wzwmzm <wzwmzm41@gmail.com>
2020-08-30 11:11:55 +0800

    a

--- 1HTML5/02-FeHelper-油猴-腾讯视频关闭静音/demo.js
+++ 1HTML5/02-FeHelper-油猴-腾讯视频关闭静音/demo.js
@@ -6,7 +6,7 @@
 // @enable          true
 // @require-js      
 // @auto-refresh    0
-// @updated         2020-08-30 10:29:59
+// @updated         2020-08-30 11:09:30
 // ==/FeHelperMonkey==
 
 
@@ -14,34 +14,30 @@
   
   $(document).ready(function(){
     
-    console.log("油猴开始了1");
-    console.log("油猴开始了1");
-    console.log("油猴开始了1");
-    console.log("油猴开始了1");
-    console.log("油猴开始了1");
-    console.log("油猴开始了1");
-    
-      
-    
+    console.log("油猴开始了1111");
+    console.log("油猴开始了----");
+    console.log("油猴开始了----");    
 
 });
   
   $(()=>{
-	console.log("油猴开始了");
-    console.log("油猴开始了");
-    console.log("油猴开始了");
-    console.log("油猴开始了");
-    console.log("油猴开始了");
-    console.log("油猴开始了");
+    console.log("油猴开始了22222");
+    console.log("油猴开始了=====");
+    console.log("油猴开始了=====");
+    console.log("油猴开始了=====");
+    
   })
   
   
     var sleep = ms => new Promise((resolve,reject) => setTimeout(resolve,ms));
   
-    sleep(3000)
-      .then(() => $('txpdiv.txp_btn.txp_btn_volume').click() ); //在页面载入完成后再过3秒关闭静音
+    sleep(1000).then(() => $('txpdiv.txp_btn.txp_btn_volume').click() ); //在页面载入完成后再过3秒关闭静音
   
   
-  //$('txpdiv.txp_btn.txp_btn_volume').click();	//在页面载入完成后关闭静音
+    console.log("油猴开始了33333");
+    console.log("油猴开始了!!!!!");
+    console.log("油猴开始了!!!!!!");
+  
+ 	//$('txpdiv.txp_btn.txp_btn_volume').click();	//在页面载入完成后关闭静音
 
 })();
