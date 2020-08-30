9901047f2b3d30a74443fff814814b2587049fd0
wzwmzm <wzwmzm41@gmail.com>
2020-08-30 10:36:35 +0800

    a

--- 1HTML5/02-FeHelper-油猴-腾讯视频关闭静音/demo.js
+++ 1HTML5/02-FeHelper-油猴-腾讯视频关闭静音/demo.js
@@ -6,7 +6,7 @@
 // @enable          true
 // @require-js      
 // @auto-refresh    0
-// @updated         2020-08-29 18:00:33
+// @updated         2020-08-30 10:29:59
 // ==/FeHelperMonkey==
 
 
@@ -21,7 +21,7 @@
     console.log("油猴开始了1");
     console.log("油猴开始了1");
     
-      $('txpdiv.txp_btn.txp_btn_volume').click();	//在页面载入完成后关闭静音
+      
     
 
 });
@@ -34,5 +34,14 @@
     console.log("油猴开始了");
     console.log("油猴开始了");
   })
+  
+  
+    var sleep = ms => new Promise((resolve,reject) => setTimeout(resolve,ms));
+  
+    sleep(3000)
+      .then(() => $('txpdiv.txp_btn.txp_btn_volume').click() ); //在页面载入完成后再过3秒关闭静音
+  
+  
+  //$('txpdiv.txp_btn.txp_btn_volume').click();	//在页面载入完成后关闭静音
 
 })();
